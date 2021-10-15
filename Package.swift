4619b7c8a91a94a2a8e998629cde26924b0acc73
rahman <rahman@bdg.zingmobile.co.id>
2021-10-15 14:54:00 +0700

    a

--- Package.swift
+++ Package.swift
@@ -3,18 +3,18 @@
 import PackageDescription
 
 let package = Package(
-  name: "testiossdk",
+  name: "testsdk",
   products: [
     .library(
-      name: "testiossdk",
-      targets: ["testiossdk"]
+      name: "testsdk",
+      targets: ["testsdk"]
     ),
   ],
   dependencies: [],
   targets: [
 	  .binaryTarget(
-	    name: "testiossdk",
-	    url: "https://github.com/topkekmann/testiossdk/releases/download/1.4.6/testsdk.xcframework.zip",
+	    name: "testsdk",
+	    url: "https://github.com/topkekmann/testiossdk/releases/download/1.4.7/testsdk.xcframework.zip",
 	    checksum: "4d0beee2973877f04b122c9529881da69b6f8d37d8ef6ba9edd1284461354093")
 	]
 )
