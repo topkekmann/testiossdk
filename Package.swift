f9ef25cfe64298f4da8c39ea7d670b33ab99d68f
rahman <rahman@bdg.zingmobile.co.id>
2021-10-15 13:50:16 +0700

    ok

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "testiossdk",
-            targets: ["testiossdk"])
+            targets: ["testiossdk"]),
     ],
    targets: [
 	  .binaryTarget(
